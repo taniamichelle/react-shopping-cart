f3846223c4f609c5bdec1f38b8c60f76f22fbf10
tania <tanislavens@yahoo.com>
2019-08-12 17:19:52 -0400

    completed MVP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
-import {ProductContext} from '../contexts/ProductContext';
-import {CartContext} from '../contexts/CartContext';
 
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import {ProductContext} from './contexts/ProductContext';
+import {CartContext} from './contexts/CartContext';
+
 
 function App() {
 	const [products] = useState(data);
